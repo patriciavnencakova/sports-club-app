e3473e28682ce300f336ee8b9bba9de86890d357
patriciavnencakova <patriciaa.vnencakova@gmail.com>
2024-05-13 04:22:36 +0200

    Remove unnecessary files

--- mysite/attendance_tracker/urls.py
+++ mysite/attendance_tracker/urls.py
@@ -7,8 +7,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path("registration/", views.registration, name="registration"),
-    # path("pos/", views.pos, name="pos"),
-    path("login/", views.login, name="login"),
-    # path('admin_page/', views.admin_page, name="admin_page"),
 ]
